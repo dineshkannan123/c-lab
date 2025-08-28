2e6f61696460b0d7d30281669426e57e20d98fc6
dineshkannan123 <dinekannan08220@gmail.com>
2025-08-28 00:59:41 -0700

    Add files via upload

--- mark sheet.c
+++ mark sheet.c
@@ -6,17 +6,23 @@ int main()
 	float tot,avg;
 	printf("\n\t\t\t\t\tBCA-A\t\t\t\t\t\n");
 	printf("\n_________________________________________________________________");
-	printf("\nName:G.DINESH KANNAN\t\t\t\t\t,Reg=11253408");
-	printf("\nTamil=%5.2f",m[0]);
-	printf("\nEnglish=%5.2f",m[1]);
-	printf("\nMathematics=%5.2f",m[2]);
-	printf("\nScience=%5.2f",m[3]);
-	printf("\nSocial science=%5.2f",m[4]);
+	printf("\nName:G.DINESH KANNAN\t\t\tReg=11253408");
+	printf("\n_________________________________________________________________");
+	printf("\nTamil\t\t\t\t\t\t%5.2f\t\t\t\t\t",m[0]);
+	printf("\nEnglish\t\t\t\t\t\t%5.2f\t\t\t\t\t",m[1]);
+	printf("\nMathematics\t\t\t\t\t%5.2f\t\t\t\t\t",m[2]);
+	printf("\nScience\t\t\t\t\t\t%5.2f\t\t\t\t\t",m[3]);
+	printf("\nSocial science\t\t\t\t\t%5.2f\t\t\t\t\t",m[4]);
 	tot=m[0]+m[1]+m[2]+m[3]+m[4];
 	avg=tot/5;
 	printf("\n_________________________________________________________________");
-	printf("\ntot=%f\t\t\t\t\tavg=%5.2f",tot,avg);
+	printf("\ntot=%5.2f\t\t\t\t\tavg=%5.2f",tot,avg);
 	printf("\n_________________________________________________________________");
-	getch();
+	if("Average>=50")
+	printf("\nResult:PASS");
+	else
+	printf("\nResult:FAIL");
 	
+	getch();
+	return 0;
 }
