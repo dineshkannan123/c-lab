c9ac58dcbfb529bfd2079d5bc49145ee300c683d
dineshkannan123 <dinekannan08220@gmail.com>
2025-08-28 00:03:48 -0700

    Add files via upload

---
+++ mark sheet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float m[5]={55,66,77,88,99};
+	float tot,avg;
+	printf("\n\t\t\t\t\tBCA-A\t\t\t\t\t\n");
+	printf("\n_________________________________________________________________");
+	printf("\nName:G.DINESH KANNAN\t\t\t\t\t,Reg=11253408");
+	printf("\nTamil=%5.2f",m[0]);
+	printf("\nEnglish=%5.2f",m[1]);
+	printf("\nMathematics=%5.2f",m[2]);
+	printf("\nScience=%5.2f",m[3]);
+	printf("\nSocial science=%5.2f",m[4]);
+	tot=m[0]+m[1]+m[2]+m[3]+m[4];
+	avg=tot/5;
+	printf("\n_________________________________________________________________");
+	printf("\ntot=%f\t\t\t\t\tavg=%5.2f",tot,avg);
+	printf("\n_________________________________________________________________");
+	getch();
+	
+}
