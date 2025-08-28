b04f1fc8722d8946df981a11b7c6643b62b040b0
dineshkannan123 <dinekannan08220@gmail.com>
2025-08-28 01:00:10 -0700

    Add files via upload

---
+++ dk.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+	
+	printf("\naddition of a+b=%d",5+8);
+	printf("\nsubtraction of a-b=%d",5-8);
+	printf("\nmultiplication of a*b=%d",5*8);
+	printf("\ndivision of a/b=%d",5/8);
+	getch();
+}
